45a68aa2a5fb71c068fd019a2db8f29fac234c9d
Jason Bobich <jasonbobich@gmail.com>
2017-06-21 13:59:33 -0600

    Use flexbox for nav menus

--- src/js/menu.js
+++ src/js/menu.js
@@ -27,7 +27,9 @@
 
 		$this.options.dropdown_selector = 'ul:not(.mega-sub-menu), .fs-mega';
 
-		$menu.find($this.options.dropdown_selector).css('display', 'none');
+		Menu.TIMER[ $this.options.id ] = [];
+
+		$menu.removeClass('no-js');
 
 		$menu.find('li').has($this.options.dropdown_selector)
 			.addClass('menu-item-has-children')
@@ -59,8 +61,7 @@
 	}
 
 	Menu.DEFAULTS = {
-		delay	: 500,
-		speed	: 200
+		delay: 500
 	}
 
 	Menu.COUNT = 0; // Assign a unique ID to each menu.
@@ -93,29 +94,64 @@
 			$flyout.removeClass('reverse');
 		}
 
-	}
+	};
 
 	Menu.prototype.show = function($trigger, options) {
 
-		clearTimeout( Menu.TIMER[ options.id ] );
+		var level = Menu.prototype.getLevel($trigger),
+			$siblings = $trigger.siblings().find(options.dropdown_selector),
+			$target = $trigger.children(options.dropdown_selector);
 
-		$trigger.siblings().find(options.dropdown_selector).fadeOut(options.speed);
+		clearTimeout( Menu.TIMER[ options.id ][ level ] );
 
-		$trigger.children(options.dropdown_selector).stop(true, true).fadeIn(options.speed);
+		$siblings.removeClass('in');
 
-	}
+		$target.addClass('show fade');
+
+		Menu.TIMER[ options.id ][ level ] = setTimeout(function() {
+
+			$siblings.removeClass('show fade');
+
+			$target.addClass('in');
+
+		}, 200);
+
+	};
 
 	Menu.prototype.hide = function($trigger, options) {
 
-		var $flyout = $trigger.children(options.dropdown_selector);
+		var level = Menu.prototype.getLevel($trigger),
+			$flyout = $trigger.children(options.dropdown_selector);
 
-		$flyout.stop(true, true).css('display', 'block');
+		clearTimeout( Menu.TIMER[ options.id ][ level ] );
 
-		Menu.TIMER[ options.id ] = setTimeout(function() {
-			$flyout.fadeOut(options.speed);
+		Menu.TIMER[ options.id ][ level ] = setTimeout(function() {
+
+			$flyout.removeClass('in');
+
+			setTimeout(function() {
+
+				$flyout.removeClass('show fade');
+
+			}, 200);
+
 		}, options.delay);
 
-	}
+	};
+
+	Menu.prototype.getLevel = function($trigger) {
+
+		var level = 'level-1';
+
+		if ( $trigger.hasClass('level-2') ) {
+			level = 'level-2';
+		} else if ( $trigger.hasClass('level-3') ) {
+			level = 'level-3';
+		}
+
+		return level;
+
+	};
 
 	// MENU PLUGIN DEFINITION
 	// =======================
