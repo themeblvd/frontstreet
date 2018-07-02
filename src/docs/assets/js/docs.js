b0640231e545d0044dab4a73f876ea41cfb6b509
Jason Bobich <jasonbobich@gmail.com>
2018-07-01 20:29:44 -0600

    Initial re-structure of docs JavaScript. #9

--- src/docs/assets/js/docs.js
+++ src/docs/assets/js/docs.js
@@ -1,167 +1,162 @@
-jQuery( document ).ready( function( $ ) {
+import $ from 'jquery';
+import Fuse from 'fuse.js';
+import searchIndex from './search-index';
+import 'owl.carousel';
+import 'magnific-popup';
+import '../../../js/main';
 
-	// Site Search
-	var options = {
-		shouldSort: true,
-		includeScore: true,
-		// tokenize: true,
-  		// matchAllTokens: true,
-		threshold: 0.3,
-		location: 0,
-		distance: 100,
-		maxPatternLength: 32,
-		minMatchCharLength: 3,
-		keys: [{
-			name: 'title',
-			weight: 0.9
-		}, {
-			name: 'tags',
-			weight: 0.1
-		}]
-	};
+$(document).ready(function($) {
+  // Site Search
+  var options = {
+    shouldSort: true,
+    includeScore: true,
+    // tokenize: true,
+    // matchAllTokens: true,
+    threshold: 0.3,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 3,
+    keys: [
+      {
+        name: 'title',
+        weight: 0.9
+      },
+      {
+        name: 'tags',
+        weight: 0.1
+      }
+    ]
+  };
 
-	var fuse = new Fuse(FrontStreetIndex, options),
-		$searchField = $('#site-search-field'),
-		$resultsList = $('#search-results > ul'),
-		$selected = null;
+  var fuse = new Fuse(searchIndex, options),
+    $searchField = $('#site-search-field'),
+    $resultsList = $('#search-results > ul'),
+    $selected = null;
 
-	$searchField.on('keyup', function(e) {
+  $searchField.on('keyup', function(e) {
+    // Is up, down, or enter being pressed?
+    if (e.which == 13 || e.which == 38 || e.which == 40) {
+      return;
+    }
 
-		// Is up, down, or enter being pressed?
-		if ( e.which == 13 || e.which == 38 || e.which == 40 ) {
-			return;
-		}
+    // $selection = null;
 
-		$selection = null;
+    var $el = $(this),
+      results = fuse.search($el.val());
 
-		var $el = $(this),
-			results = fuse.search( $el.val() );
+    $resultsList.empty();
 
-		$resultsList.empty();
+    for (var i = 0; i < results.length; i++) {
+      $resultsList.append(
+        '<li><a href="' +
+          results[i].item.url +
+          '" data-score="' +
+          results[i].score +
+          '">' +
+          results[i].item.title +
+          '</a></li>'
+      );
+    }
+  });
 
-		for ( var i = 0; i < results.length; i++ ) {
-			$resultsList.append('<li><a href="' + results[i].item.url + '" data-score="' + results[i].score + '">' + results[i].item.title + '</a></li>');
-		}
+  $searchField.on('keydown', function(e) {
+    var enter = e.which == 13,
+      up = e.which == 38,
+      down = e.which == 40;
 
+    if (down) {
+      if (!$selected) {
+        $selected = $resultsList.find('li:first-child');
+      } else {
+        $selected = $selected.next();
+      }
 
-	});
+      // If there was no next(), then go back to first item.
+      if (!$selected.length) {
+        $selected = $resultsList.find('li:first-child');
+      }
+    } else if (up) {
+      if (!$selected) {
+        $selected = $resultsList.find('li:last-child');
+      } else {
+        $selected = $selected.prev();
+      }
 
-	$searchField.on('keydown', function(e) {
+      // If there was no prev(), then go back to last item.
+      if (!$selected.length) {
+        $selected = $resultsList.find('li:last-child');
+      }
+    } else if (enter) {
+      if ($selected) {
+        var url = $selected.find('a').attr('href');
 
-		var enter	= e.which == 13,
-			up		= e.which == 38,
-			down	= e.which == 40;
+        $selected = null;
+        $resultsList.empty();
+        $searchField.val('');
 
-		if (down) {
+        window.location.href = url;
+      }
+    }
 
-			if ( ! $selected ) {
-				$selected = $resultsList.find('li:first-child');
-			} else {
-				$selected = $selected.next();
-			}
+    if (up || down) {
+      $resultsList.find('li').removeClass('selected');
+      $selected.addClass('selected');
+    }
+  });
 
-			// If there was no next(), then go back to first item.
-			if ( ! $selected.length ) {
-				$selected = $resultsList.find('li:first-child');
-			}
+  $resultsList.on('click', 'a', function() {
+    $searchField.val('');
+    $resultsList.stop(true, true).fadeOut(100);
+  });
 
-		} else if (up) {
+  $searchField.on('focus', function() {
+    $resultsList.stop(true, true).fadeIn(100);
+  });
 
-			if ( ! $selected ) {
-				$selected = $resultsList.find('li:last-child');
-			} else {
-				$selected = $selected.prev();
-			}
+  $searchField.on('focusout', function() {
+    setTimeout(function() {
+      $resultsList.stop(true, true).fadeOut(100);
+    }, 500);
+  });
 
-			// If there was no prev(), then go back to last item.
-			if ( ! $selected.length ) {
-				$selected = $resultsList.find('li:last-child');
-			}
+  // Scroll To Top
+  var $toTop = $('.to-top');
 
-		} else if (enter) {
+  $toTop.on('click', function() {
+    $('html, body').animate({ scrollTop: 0 }, 400);
+    return false;
+  });
 
-			if ( $selected ) {
+  $(window).on('scroll load', function() {
+    var sidebarHeight = $('.site-sidebar > .wrap').outerHeight();
+    var btnHeight = $toTop.outerHeight();
 
-				var url = $selected.find('a').attr('href');
+    if ($(this).scrollTop() >= sidebarHeight - btnHeight) {
+      $toTop.addClass('stick');
+    } else {
+      $toTop.removeClass('stick');
+    }
+  });
 
-				$selected = null;
-				$resultsList.empty();
-				$searchField.val('');
+  // Menu Toggle examples
 
-				window.location.href = url;
+  $('.fs-menu-toggle').on('click', function() {
+    var $el = $(this);
 
-			}
+    if ($el.hasClass('collapse')) {
+      $el.removeClass('collapse');
 
-		}
+      // Code to close your menu...
+    } else {
+      $el.addClass('collapse');
 
-		if ( up || down ) {
-			$resultsList.find('li').removeClass('selected');
-			$selected.addClass('selected');
-		}
+      // Code to open your menu...
+    }
 
-	});
+    return false;
+  });
 
-	$resultsList.on('click', 'a', function() {
-		$searchField.val('');
-		$resultsList.stop(true, true).fadeOut(100);
-	});
-
-	$searchField.on('focus', function() {
-		$resultsList.stop(true, true).fadeIn(100);
-	});
-
-	$searchField.on('focusout', function() {
-		setTimeout(function() {
-			$resultsList.stop(true, true).fadeOut(100);
-		}, 500);
-	});
-
-	// Scroll To Top
-	var $toTop = $('.to-top');
-
-	$toTop.on('click', function() {
-		$('html, body').animate({scrollTop : 0}, 400);
-		return false;
-	});
-
-	$(window).on('scroll load', function() {
-
-		var sidebarHeight = $('.site-sidebar > .wrap').outerHeight();
-			btnHeight = $toTop.outerHeight();
-
-		if ( $(this).scrollTop() >= (sidebarHeight - btnHeight) ) {
-			$toTop.addClass('stick');
-		} else {
-			$toTop.removeClass('stick');
-		}
-
-	});
-
-	// Menu Toggle examples
-
-	$('.fs-menu-toggle').on('click', function(){
-
-		var $el = $(this);
-
-		if ( $el.hasClass('collapse') ) {
-
-			$el.removeClass('collapse');
-
-			// Code to close your menu...
-
-		} else {
-
-			$el.addClass('collapse');
-
-			// Code to open your menu...
-
-		}
-
-		return false;
-
-	});
-
-	// Moble Menu examples
-	$( '.mobile-menu-bar' ).frontStreet( 'mobile-menu' );
-
+  // Moble Menu examples
+  $('.mobile-menu-bar').frontStreet('mobile-menu');
 });
